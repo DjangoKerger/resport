70656c241496d646187d0a75d0dfc0626b7117cf
“First <Kerger06@aol.com>
2021-12-31 16:51:23 -0600

    info section started, need to figure out why picture is not showing

---
+++ src/components/InfoSection/Data.js
@@ -0,0 +1,15 @@
+export const homeObjOne = {
+    id: 'about',
+    lightBg: false,
+    lightText: true,
+    lightTextDesc: true,
+    topLine: 'Premium Bank',
+    headline: 'Unlimited Transactions with 0 fee',
+    description: 'Get access to out exclusive app that allows you to send unlimited transactions without getting charged any fees',
+    buttonLabel: 'Get started',
+    imgStart: false,
+    img: require('../../images/svg1.svg'),
+    alt: 'Car',
+    dark: true,
+    darkText: false
+}
